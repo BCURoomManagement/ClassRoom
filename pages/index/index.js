ad34ecbc47e98ac991866f33e7acd67ec4f8abf6
EZJJJJ <1638623039@qq.com>
2018-10-04 01:37:45 +0800

    logn

--- pages/index/index.js
+++ pages/index/index.js
@@ -56,53 +56,53 @@ Page({
   //登录
   formBindsubmit: function (res){
     var that=this;
-    wx.request({
-      url: API_URL + "?userName=" + res.detail.value.userName + "&&userPwd=" + res.detail.value.userPwd,
-      header: {
-        "Content-Type": "json"
-      },
-      data: {},
-      method: 'GET',
-      success: function (e) {
-        console.log(e)
-        if (e.data["0"].userDep == "") {
-          wx.showModal({
-            content: '用户名和密码不正确，请重新输入！',
-            title: '提示',
-            showCancel: false,
-            confirmText: '确定',
-            confirmColor: "#77a9fb"
-          })
-        }
-        else {
-          // app.Appusername.userinfo = { UN: e.data["0"].userName, UI: e.data["0"].userId, UP: e.data["0"].userPwd, UD: e.data["0"].userDep, UT: e.data["0"].userType, US: e.data["0"].userStatus }
-          //登陆成功
-          that.setData({
-            flag: true,
-            username: null,
-            department: null,
-            studentnumber: null,
-            islogin: true,
-          })
-        }
-      },
-      fail: function (e) {
-        that.setData({
-          flag: true,
-          username: '呆霸王',
-          department: "信息",
-          studentnumber: 123456,
-          islogin: true,
-        })
-        // wx.showModal({
-        //   title: '提示',
-        //   content: '连接服务器失败，请稍后再试！',
-        //   showCancel: false,
-        //   confirmText: '确定',
-        //   confirmColor: "#77a9fb"
-        // })
-      },
-    })
+    // wx.request({
+    //     url: "http://localhost:8080/LognServlet" + "?userName=" + res.detail.value.userName + "&&userPwd=" + res.detail.value.userPwd,
+    //   header: {
+    //     "Content-Type": "json"
+    //   },
+    //   data: {},
+    //   method: 'GET',
+    //   success: function (e) {
+    //     console.log(e)
+    //     if (e.data["0"].userDep == "") {
+    //       wx.showModal({
+    //         content: '用户名和密码不正确，请重新输入！',
+    //         title: '提示',
+    //         showCancel: false,
+    //         confirmText: '确定',
+    //         confirmColor: "#77a9fb"
+    //       })
+    //     }
+    //     else {
+    //       // app.Appusername.userinfo = { UN: e.data["0"].userName, UI: e.data["0"].userId, UP: e.data["0"].userPwd, UD: e.data["0"].userDep, UT: e.data["0"].userType, US: e.data["0"].userStatus }
+    //       //登陆成功
+    //       that.setData({
+    //         flag: true,
+    //         username: null,
+    //         department: null,
+    //         studentnumber: null,
+    //         islogin: true,
+    //       })
+    //     }
+    //   },
+    //   fail: function (e) {
+    //     that.setData({
+    //       flag: true,
+    //       username: '呆霸王',
+    //       department: "信息",
+    //       studentnumber: 123456,
+    //       islogin: true,
+    //     })
+    //     // wx.showModal({
+    //     //   title: '提示',
+    //     //   content: '连接服务器失败，请稍后再试！',
+    //     //   showCancel: false,
+    //     //   confirmText: '确定',
+    //     //   confirmColor: "#77a9fb"
+    //     // })
+    //   },
+    // })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
