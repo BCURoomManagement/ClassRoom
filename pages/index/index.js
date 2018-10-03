01129e8e904c33852deae64f01f48defba489b32
daibawang <873027348@qq.com>
2018-10-03 17:07:35 +0800

    登录

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
-const app = getApp()
+var app = getApp();
+var API_URL = "";
 // @需要字段:用户姓名username  学部department 学号studentnumber
 // 学号可以为空
 Page({
@@ -8,9 +9,11 @@ Page({
    */
   data: {
     userInfo: {},
-    username:"呆霸王",
-    department:'信息学部',
-    studentnumber: 16111651125,
+    username:null,
+    department:null,
+    studentnumber: null,
+    islogin:false,
+    flag:true,
   },
 
   /**
@@ -24,8 +27,80 @@ Page({
     })
   },
   go: function (event) {
-    wx.redirectTo({
-      url: event.currentTarget.dataset.href
+    if (this.data.islogin==false){
+      wx.showModal({
+        content: '请先登录',
+        showCancel: false,
+        confirmText: '确定',
+        confirmColor: "#77a9fb"
+      })
+    }else{
+      wx.redirectTo({
+        url: event.currentTarget.dataset.href
+      })
+    }
+
+  },
+  //登录弹出层
+  login: function (e) {
+    this.setData({
+      flag: false,
+    })
+  },
+  hide: function () {
+    this.setData({
+      flag: true,
+    })
+  },
+  //登录
+  formBindsubmit: function (res){
+    var that=this;
+    wx.request({
+      url: API_URL + "?userName=" + res.detail.value.userName + "&&userPwd=" + res.detail.value.userPwd,
+      header: {
+        "Content-Type": "json"
+      },
+      data: {},
+      method: 'GET',
+      success: function (e) {
+        console.log(e)
+        if (e.data["0"].userDep == "") {
+          wx.showModal({
+            content: '用户名和密码不正确，请重新输入！',
+            title: '提示',
+            showCancel: false,
+            confirmText: '确定',
+            confirmColor: "#77a9fb"
+          })
+        }
+        else {
+          // app.Appusername.userinfo = { UN: e.data["0"].userName, UI: e.data["0"].userId, UP: e.data["0"].userPwd, UD: e.data["0"].userDep, UT: e.data["0"].userType, US: e.data["0"].userStatus }
+          //登陆成功
+          that.setData({
+            flag: true,
+            username: null,
+            department: null,
+            studentnumber: null,
+            islogin: true,
+          })
+        }
+      },
+      fail: function (e) {
+        that.setData({
+          flag: true,
+          username: '呆霸王',
+          department: "信息",
+          studentnumber: 123456,
+          islogin: true,
+        })
+        // wx.showModal({
+        //   title: '提示',
+        //   content: '连接服务器失败，请稍后再试！',
+        //   showCancel: false,
+        //   confirmText: '确定',
+        //   confirmColor: "#77a9fb"
+        // })
+      },
     })
   },
   /**
